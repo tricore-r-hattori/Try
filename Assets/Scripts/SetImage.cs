01d4e740d10d972a6fe3ccfb18c55e2dbd9fe470
reooo <19050037hr@edu.fca.ac.jp>
2021-07-20 01:03:15 +0900

    画像切り替え処理とスクロール処理実装

---
+++ Assets/Scripts/SetImage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SetImage : MonoBehaviour
+{
+    [SerializeField]
+    Image image;
+
+    [SerializeField]
+    RectTransform imageTransform;
+
+    [SerializeField]
+    Sprite sprite;
+
+    const float imageChangePoint = 320;
+
+    void Update()
+    {
+        if (imageTransform.localPosition.y >= imageChangePoint)
+        {
+            image.sprite = sprite;
+        }
+    }
+}
