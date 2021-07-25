5f789ab6ec31eab23754dfdc4a92f25d70fd8b28
reooo <19050037hr@edu.fca.ac.jp>
2021-07-25 22:45:32 +0900

    低確率でレア瓦をだすための確率によるイベント処理を実装

---
+++ Assets/Scripts/Probability.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Probability : MonoBehaviour
+{
+    [SerializeField]
+    List<Image> myList = new List<Image>(); //Listの宣言
+    Image rareTile;
+
+    [SerializeField]
+    Sprite sprite;
+
+    void Start()
+    {
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    myList.Add(i); //Listに値を格納
+        //}
+
+        if (Probability_(10))
+        {
+            rareTile.sprite = sprite;
+        }
+    }
+
+    /// <summary>
+    /// 確率判定
+    /// </summary>
+    /// <param name="fPercent">確率 (0~100)</param>
+    /// <returns>当選結果 [true]当選</returns>
+    bool Probability_(float fPercent)
+    {
+        float fProbabilityRate = UnityEngine.Random.value * 100.0f;
+
+        if (fPercent == 100.0f && fProbabilityRate == fPercent)
+        {
+            return true;
+        }
+        else if (fProbabilityRate < fPercent)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
