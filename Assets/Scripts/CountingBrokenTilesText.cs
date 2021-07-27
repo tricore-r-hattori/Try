39f78533f923da39e9169267f81776990c0bfe95
reooo <19050037hr@edu.fca.ac.jp>
2021-07-28 00:48:47 +0900

    割った瓦をカウントする処理実装

---
+++ Assets/Scripts/CountingBrokenTilesText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CountingBrokenTilesText : MonoBehaviour
+{
+    [SerializeField]
+    Text Count = default;
+
+    [SerializeField]
+    List<TileImageChanger> tile = new List<TileImageChanger>();
+
+    int brokenTilesCount = 0;
+
+    void OnEnable()
+    {
+        brokenTilesCount = 0;
+
+        for (int i = 0; i < tile.Count; i++)
+        {
+            tile[i].A += Method;
+        }
+    }
+
+    void OnDisable()
+    {
+        for (int i = 0; i < tile.Count; i++)
+        {
+            tile[i].A -= Method;
+        }
+    }
+
+    public void Method()
+    {
+        brokenTilesCount++;
+
+        Count.text = brokenTilesCount.ToString();
+
+        Debug.Log(brokenTilesCount);
+    }
+}
